ba607e8f8069caa7a55d5f997760207e369ab6f8
Xiangling Lan <531687686@qq.com>
2018-03-24 20:19:50 +0800

    test php

--- area.php
+++ area.php
@@ -1,7 +1,7 @@
 <?php
-header('content-type:text/html;charset=utf-8');
+header('content-type:text/html; charset=utf-8');
 $str=file_get_contents('data/sec.json');
-$arr=json_decode('$str');
+$arr=json_decode($str);
 $obj=$arr[$_GET['index']];
 echo json_encode($obj);
 ?>
