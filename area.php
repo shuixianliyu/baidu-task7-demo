4e479aa97d5bfdf174a1018043261541e32b3ba1
Xiangling Lan <531687686@qq.com>
2018-03-24 20:21:48 +0800

    test php

--- area.php
+++ area.php
@@ -2,6 +2,6 @@
 header('content-type:text/html; charset=utf-8');
 $str=file_get_contents('data/sec.json');
 $arr=json_decode($str);
-$obj=$arr[$_GET['index']];
+$obj=$arr[5];
 echo json_encode($obj);
 ?>
