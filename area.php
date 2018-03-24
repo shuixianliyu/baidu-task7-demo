b3ac5897aa584b6b989793e14d5e40e9d7e42bb5
Xiangling Lan <531687686@qq.com>
2018-03-24 20:50:37 +0800

    test php

--- area.php
+++ area.php
@@ -1,9 +1,7 @@
 <?php
-header('content-type:text/html;charset=utf-8');
-    // 从文件中读取数据到PHP变量
-   $json_string = file_get_contents('data/sec.json');
-   // 用参数true把JSON字符串强制转成PHP数组
-   $arr = json_decode($json_string, true);
-   $obj=$arr[$_GET['index']];
-   echo json_encode($obj);
+    header('content-type:text/html; charset=utf-8');
+    $json_string = file_get_contents('data/sec.json');
+    $arr = json_decode($json_string, true);
+    $obj=$arr[$_GET['index']];
+    echo json_encode($obj);
 ?>
